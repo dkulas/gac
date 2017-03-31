de1660381e83378d7ad94e29c40a5550b3543cef
Danny Kulas <dkulas@cogentibs.com>
2017-03-31 12:09:27 -0400

    added some error handling to the appjs file for the fetch api call

--- js/app.js
+++ js/app.js
@@ -1,5 +1,13 @@
 var submitBtn = document.getElementById("submitBtn");
 
+(function checkFetch() {
+  if (self.fetch) {
+    console.log("Fetch is working");
+  } else {
+    alert("Something happened...it's not you, well it might be.  Check your browser settings and make sure it supports the fetch() API call.  Otherwise, please upgrade to the latest and greatest version of Chrome or FireFox");
+  }
+})();
+
 function fetchData() {
   fetch("https://api.github.com/users/" + getUsername())
   .then(data => data.json())
